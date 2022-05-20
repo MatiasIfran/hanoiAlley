c893b04bcc0cb3c14afbfd2b46c594182cab1c5b
Matias Ifran <ifran2855@gmail.com>
2022-05-20 20:03:11 -0300

    arreglos

--- wp-content/themes/gloriafood-restaurant/header.php
+++ wp-content/themes/gloriafood-restaurant/header.php
@@ -17,13 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="profile" href="https://gmpg.org/xfn/11"/>
 	<?php wp_head(); ?>
-	<script>
-    jQuery(function($){
-      $('.nav-link').click(function() {
-          $('#navbarNav').collapse('hide')
-      });
-    });
-  </script>
+	
 </head>
 
 <body <?php body_class(); ?>>
